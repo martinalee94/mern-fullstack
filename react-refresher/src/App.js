8e9a5be18bc68e196c54e19a57d8c28cfbdde355
martinalee94 <martinalee0528@gmail.com>
2023-07-26 05:22:15 +0900

    React refresher - component

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import GoalList from './components/GoalList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return React.createElement('h1',{}, 'Hi, this is react!');
+  // JSX
+  return <div className="course-goals">
+  <h1 title="Course Goals">Course Goals</h1>
+  <GoalList />
+  </div>;
 }
 
 export default App;
---
+++ react-refresher/src/components/GoalList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './GoalList.css';
+
+const GoalList = () =>{
+    return (<ul className='goal-list'>
+    <li>
+      Finish the Course
+    </li>
+    <li>
+      Course Main Topic
+    </li>
+    <li>
+      Help other students Q&A
+    </li>
+  </ul>);
+}
+
+export default GoalList;
