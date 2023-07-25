4147065edfc6020e0e5e51ecccc80343a18d2d6f
martinalee94 <martinalee0528@gmail.com>
2023-07-26 05:39:35 +0900

    [#1] Props

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,15 +1,22 @@
 // import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import GoalList from './components/GoalList';
+import "./App.css";
+import React from "react";
+import GoalList from "./components/GoalList";
 
 function App() {
   // return React.createElement('h1',{}, 'Hi, this is react!');
   // JSX
-  return <div className="course-goals">
-  <h1 title="Course Goals">Course Goals</h1>
-  <GoalList />
-  </div>;
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Course Main Topic" },
+    { id: "cg3", text: "Help other students Q&A" },
+  ];
+  return (
+    <div className="course-goals">
+      <h1 title="Course Goals">Course Goals</h1>
+      <GoalList goals={courseGoals} />
+    </div>
+  );
 }
 
 export default App;
--- react-refresher/src/components/GoalList.js
+++ react-refresher/src/components/GoalList.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import './GoalList.css';
+import React from "react";
+import "./GoalList.css";
 
-const GoalList = () =>{
-    return (<ul className='goal-list'>
-    <li>
-      Finish the Course
-    </li>
-    <li>
-      Course Main Topic
-    </li>
-    <li>
-      Help other students Q&A
-    </li>
-  </ul>);
-}
+const GoalList = (props) => {
+  return (
+    <ul className="goal-list">
+      {props.goals.map((goal) => {
+        return <li key={goal.id}>{goal.text}</li>;
+      })}
+    </ul>
+  );
+};
 
 export default GoalList;
